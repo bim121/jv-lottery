df5338228b18926ca9b4faa54614b6fec0b79ec0
Vitaliy Tishik <vitisik54@gmail.com>
2025-10-06 13:01:05 +0300

    refactor

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -12,20 +12,24 @@ public class Ball {
     public String getColor() {
         return color;
     }
+
     public int getNumber() {
         return number;
     }
+
     public void setColor(String color) {
         this.color = color;
     }
+
     public void setNumber(int number) {
         this.number = number;
     }
+
     @Override
     public String toString() {
         return "Ball{" +
-                "color='" + color + '\'' +
-                ", number=" + number +
-                '}';
+                "color='" + color + '\''
+                + ", number=" + number
+                + '}';
     }
 }
