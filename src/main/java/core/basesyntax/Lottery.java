ec1285516e9b6f8d05aea2579f216baece27ab86
Vitaliy Tishik <vitisik54@gmail.com>
2025-10-06 12:58:32 +0300

    Add Ball and Lottery class

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    public static Ball getRandomBall() {
+        Random random = new Random();
+        String randomColor = ColorSupplier.getRandomColor();
+        int randomNumber = random.nextInt(100) + 1;
+        return new Ball(randomColor, randomNumber);
+    }
+}
